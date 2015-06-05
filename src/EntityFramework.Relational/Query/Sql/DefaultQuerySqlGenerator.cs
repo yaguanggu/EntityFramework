d2f0dd5af545b4443cc8974aaff3318c97045550
mikary <mikary@microsoft.com>
2015-06-05 14:21:59 -0700

    Fix multiple FromSql call parameter name collision

--- src/EntityFramework.Relational/Query/Sql/DefaultQuerySqlGenerator.cs
+++ src/EntityFramework.Relational/Query/Sql/DefaultQuerySqlGenerator.cs
@@ -26,6 +26,7 @@ namespace Microsoft.Data.Entity.Relational.Query.Sql
         private IndentedStringBuilder _sql;
         private List<CommandParameter> _commandParameters;
         private IDictionary<string, object> _parameterValues;
+        private int _rawSqlParameterIndex;
 
         public DefaultQuerySqlGenerator([NotNull] SelectExpression selectExpression)
         {
@@ -41,6 +42,7 @@ namespace Microsoft.Data.Entity.Relational.Query.Sql
             _sql = new IndentedStringBuilder();
             _commandParameters = new List<CommandParameter>();
             _parameterValues = parameterValues;
+            _rawSqlParameterIndex = 0;
 
             Visit (_selectExpression);
 
@@ -233,7 +235,7 @@ namespace Microsoft.Data.Entity.Relational.Query.Sql
 
                 for (var index = 0; index < rawSqlDerivedTableExpression.Parameters.Count(); index++)
                 {
-                    var parameterName = ParameterPrefix + "p" + index;
+                    var parameterName = ParameterPrefix + "p" + _rawSqlParameterIndex++;
 
                     _commandParameters.Add(new CommandParameter(parameterName, rawSqlDerivedTableExpression.Parameters[index]));
 
